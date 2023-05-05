d85e91aed1f37d7c828118a8b9e868813535ca34
sonbuwon <bwgh0268@naver.com>
2023-05-05 22:11:33 +0900

    Jump to Spring 2 - 10 Question Detail (질문 상세)

--- src/test/java/com/mysite/sbb/SbbApplicationTests.java
+++ src/test/java/com/mysite/sbb/SbbApplicationTests.java
@@ -1,13 +1,41 @@
 package com.mysite.sbb;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.mysite.sbb.answer.Answer;
+import com.mysite.sbb.answer.AnswerRepository;
+import com.mysite.sbb.question.Question;
+import com.mysite.sbb.question.QuestionRepository;
+
+import jakarta.transaction.Transactional;
 
 @SpringBootTest
 class SbbApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+	
+	@Autowired
+    private AnswerRepository answerRepository;
+	
+	@Transactional
 	@Test
-	void contextLoads() {
+	void jpaTest() {
+		Optional<Question> oq = this.questionRepository.findById(3);
+        assertTrue(oq.isPresent());
+        Question q = oq.get();
+        
+        List<Answer> aList = q.getAnswerList();
+        Answer a = aList.get(0);
+        assertEquals(1, a.getId());
 	}
-
 }
